95018f61a1bf625d5bcb51c25157ad005fd34384
Winicius-O <winiciusoliveira3@gmail.com>
2021-11-04 11:47:03 -0300

    nayeon

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ client = commands.Bot(command_prefix = "_")
 async def on_ready():
     print("user ON")
 
-@tasks.loop(minutes=30.0)
+@tasks.loop(hours=2.0)
 async def sender(startingUp=True):
     if startingUp:
         await asyncio.sleep(5)
         startingUp = False
 
-    channel = client.get_channel(658202312862334976)
+    channel = client.get_channel(764512553392799754)
     await channel.send("$p")
 
 sender.start()
